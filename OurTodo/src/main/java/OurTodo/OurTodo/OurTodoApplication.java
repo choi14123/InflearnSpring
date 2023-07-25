c12f23cfd6b9fc5590de5b04b167b1b8580728b0
ckdghks5565@gmail.com <ckdghks5565@gmail.com>
2023-07-25 15:29:10 +0900

    add OurTodo

---
+++ OurTodo/src/main/java/OurTodo/OurTodo/OurTodoApplication.java
@@ -0,0 +1,13 @@
+package OurTodo.OurTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurTodoApplication.class, args);
+	}
+
+}
---
+++ OurTodo/src/test/java/OurTodo/OurTodo/OurTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package OurTodo.OurTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
