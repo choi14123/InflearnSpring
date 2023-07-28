c0f221c987a99a9a3ea2e5350f4eb01a1069273c
ckdghks5565@gmail.com <ckdghks5565@gmail.com>
2023-07-28 13:42:15 +0900

    add RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -2,8 +2,7 @@ package hello.springmvc.basic;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -22,6 +21,5 @@ public class LogTestController {
         log.warn("warn log = {} ", name);
         log.error("error log = {} ", name);
         return "ok";
-
     }
 }
